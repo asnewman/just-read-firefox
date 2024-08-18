c53bc5dacef955060f2baeccc3254c16de894681
masukomi <masukomi@masukomi.org>
2024-08-18 08:35:17 -0400

    removed utterance speed options
    instead just relying on the speed
    set at the OS level

--- content.js
+++ content.js
@@ -2,8 +2,5 @@ browser.runtime.onMessage.addListener((message) => {
   const synth = window.speechSynthesis;
   const utterance = new SpeechSynthesisUtterance(message.text);
 
-  browser.storage.local.get("utterance-speed").then((result) => {
-    utterance.rate = result["utterance-speed"];
-    synth.speak(utterance);
-  });
+  synth.speak(utterance);
 });
